ca34496b92e1abe447caed7fc837f3bcb7c83620
Phong <vanphong250895@gmail.com>
2023-06-21 21:01:13 +0700

    init

---
+++ js/variables.js
@@ -0,0 +1,57 @@
+var rtl = false;
+var onepage_nav = true;
+var mobile_menu_title = "Menu";
+var hero_full_screen = true;
+var slidehow_images = ["../images/wedding_img/IMG_3803.JPG", "../images/wedding_img/IMG_3799.JPG", "../images/wedding_img/IMG_3800.JPG", "../images/wedding_img/IMG_3802.JPG", "../images/wedding_img/IMG_3804.JPG", "../images/wedding_img/IMG_3805.JPG", ];
+var timeline_parallax = true;
+var contact_form_success_msg = "Form successfully submitted! :)";
+var contact_form_error_msg = "Error sending message :(";
+var contact_form_recaptcha_error_msg = "Error verifying reCaptcha!";
+var c_days = "DAYS";
+var c_hours = "HOURS";
+var c_minutes = "MIN.";
+var c_seconds = "SEC.";
+var countdown_end_msg = "Event Started!";
+var map_canvas_id = "map_canvass";
+var map_color = "#8eaeba";
+var map_initial_zoom = 35;
+var map_initial_latitude = 33.779613;
+var map_initial_longitude = -118.066904;
+var use_default_map_style = false;
+var map_markers = [{
+	"title": "Reception",
+	"latitude": 33.775664,
+	"longitude": -118.065090,
+	"icon": "fas fa-glass-cheers",
+	"infoWindow": "Old Ranch Country Club <br> 29033 West Lake Drive, Agoura Hills, CA"
+}, {
+	"title": "Accommodation 1",
+	"latitude": 33.777929,
+	"longitude": -118.076891,
+	"icon": "fas fa-bed",
+	"infoWindow": "Accommodation 1 <br> Rossmoor, CA 90720"
+}, {
+	"title": "Accommodation 2",
+	"latitude": 33.780428,
+	"longitude": -118.084075,
+	"icon": "fas fa-bed",
+	"infoWindow": "Accommodation 2 <br> Los Alamitos, CA 90720"
+}, {
+	"title": "Accommodation 3",
+	"latitude": 33.777551,
+	"longitude": -118.050468,
+	"icon": "fas fa-bed",
+	"infoWindow": "Accommodation 3 <br> Seal Beach, CA 90740"
+}, {
+	"title": "Transportation",
+	"latitude": 33.782930,
+	"longitude": -118.060552,
+	"icon": "fas fa-plane",
+	"infoWindow": "Seal Beach VORTAC SLI <br> Los Alamitos, CA 90720"
+}, {
+	"title": "Ceremony",
+	"latitude": 33.776384,
+	"longitude": -118.058149,
+	"icon": "fas fa-bell",
+	"infoWindow": "Birchwood Church <br> Seal Beach, CA 90740"
+}];
